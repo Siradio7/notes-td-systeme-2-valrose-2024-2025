97fbf843c7dbf75453ff470c462bb9454f6da40d
Etienne Lozes <etienne.lozes@gmail.com>
2025-03-10 18:29:00 +0100

    polissage q4.shell_like.py

--- 20250310-13h/q4.shell_like.py
+++ 20250310-13h/q4.shell_like.py
@@ -1,19 +1,20 @@
 import os, sys, time, random
-#print(sys.argv)
+
 try:
     n = int(sys.argv[1])
 except:
     print("Usage: python3 q4.escalier.py <n>")
     sys.exit(1)
-# a vous!
 def affiche():
     mon_pid = os.getpid()
     pid_de_mon_pere = os.getppid()
     print(f"{mon_pid=}, {pid_de_mon_pere=}")
+
 affiche()
+
 for _ in range(n):
     if os.fork() == 0:
-        # si on veut observer le parallelisme entre les fils 
+        # décommenter la ligne ci-dessous pour observer le parallelisme entre les différents processus
         #time.sleep(random.randint(1, 5))
         affiche()
         sys.exit(0)
