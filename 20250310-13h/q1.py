7edc216d596daaaad8e96f6d806d94cd801af998
Etienne Lozes <etienne.lozes@gmail.com>
2025-03-10 18:24:00 +0100

    plus d'explications sur le lancement de programmes Python depuis un terminal

--- 20250310-13h/q1.py
+++ 20250310-13h/q1.py
@@ -1,4 +1,8 @@
 # coding: utf-8
+# ligne de dessus optionnelle, permet de spécifier l'encodage du fichier (par défaut utf-8)
+# pour exécuter ce fichier depuis un terminal
+# 1. se placer dans le bon répertoire (commande `cd`)
+# 2. taper `python q1.py`
 import os, sys
 x = 1
 mon_pid = os.fork()
