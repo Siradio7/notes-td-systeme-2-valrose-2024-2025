9fd188230efa68ce0eea2585f826ee8601456d92
Etienne Lozes <etienne.lozes@gmail.com>
2025-03-10 18:27:05 +0100

    polissage q4.escalier.py

--- 20250310-13h/q4.escalier.py
+++ 20250310-13h/q4.escalier.py
@@ -1,17 +1,23 @@
 import os, sys
-#print(sys.argv)
+
+# on récupère le nombre de forks à effectuer dans la ligne de commande
+# exemple `python3 q4.escalier.py 3`
 try:
     n = int(sys.argv[1])
 except:
     print("Usage: python3 q4.escalier.py <n>")
     sys.exit(1)
-# a vous!
+
 def affiche():
+    """Affiche le pid du processus et de son père."""
     mon_pid = os.getpid()
     pid_de_mon_pere = os.getppid()
     print(f"{mon_pid=}, {pid_de_mon_pere=}")
+    
+
 for _ in range(n):
     affiche()
     if os.fork() != 0: 
         sys.exit(0)
+
 affiche()
