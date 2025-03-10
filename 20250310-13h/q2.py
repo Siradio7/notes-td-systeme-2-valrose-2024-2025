b61eb7c81ff7f4e00bbd8b3ec07ea718c2eae32a
Etienne Lozes <etienne.lozes@gmail.com>
2025-03-10 18:21:34 +0100

    plus d'explication sur le shebang

--- 20250310-13h/q2.py
+++ 20250310-13h/q2.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 # ligne du dessus = shebang
-# rend le programme executable
+# ajouter ça et donner les droits en exécution sur le fichier
+# avec `chmod u+x q2.py` 
+# fait de ce fichier un exécutable
+# pour tester: `./q2.py` depuis un terminal
 import os, sys
 
 os.fork()
