67587585ed3757b9258a4ff2c91d7084c41db938
Etienne Lozes <elozes@unice.fr>
2025-03-24 15:25:15 +0100

    ajout du message d'usage pour execcmd

--- 20250324-13h/execcmd.py
+++ 20250324-13h/execcmd.py
@@ -4,9 +4,21 @@
 # paramètre. Exemple d’exécution :
 # ./execcmd.py /bin/ls -Ft /
 import os, sys
-print(f"[depuis execcmd] {os.getpid()}")
-chemin_absolu_fichier = sys.argv[1] #"/bin/ls"
-argv_utile = sys.argv[1:] #["/bin/ls", "-Ft", "/"]
+
+# enlever le commentaire pour observer que le pid ne change pas entre ce programme et celui exécuté
+# ./execmd.py print_pid.py
+#print(f"[depuis execcmd] {os.getpid()}")
+
+# on recupere le nom de la commande et ses arguments
+try :
+    chemin_absolu_fichier = sys.argv[1] #"/bin/ls"
+    argv_utile = sys.argv[1:] #["/bin/ls", "-Ft", "/"]
+except:
+    print("Usage: execcmd.py /bin/ls -Ft /")
+    sys.exit(1)
+
+#os.execvp(chemin_absolu_fichier, argv_utile)
+# mieux: on autorise aussi à avoir le nom de l'executable sans le chemin (on cherche dans le PATH)
 os.execvp(chemin_absolu_fichier, argv_utile)
 
 # si je veux faire croire au programme que je suis toto
