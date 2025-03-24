43e87bcc956ce5b5497ad3f3cb5f00ef227c1e60
Etienne Lozes <elozes@unice.fr>
2025-03-24 17:38:29 +0100

    fin seance 15h30

--- 20250324-15h30/ou_bit_a_bit_exemple.py
+++ 20250324-15h30/ou_bit_a_bit_exemple.py
@@ -3,7 +3,7 @@ def une_fonction_booleene_a_la_python(b1, b2, b3):
     print("b2=", b2)
     print("b3=", b3)
 
-Wb1 = 0b00000001
+Wb1 = 0b00000001 # ecriture en binaire
 Wb2 = 0b00000010
 Wb3 = 0b00000100
 
@@ -12,6 +12,7 @@ def la_meme_fonction_booleene_a_la_C(octet):
     b1 = (octet & Wb1) != 0
     b2 = (octet & Wb2) != 0
     b3 = (octet & Wb3) != 0
+    une_fonction_booleene_a_la_python(b1, b2, b3)
 
 une_fonction_booleene_a_la_python(True, False, True)
 la_meme_fonction_booleene_a_la_C(0b00000101)
